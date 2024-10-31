f29464746e45eb811ad738fcbc90d4fb1fb3f810
ied9 <ied9@tpu.ru>
2024-10-31 23:52:34 +0700

    Add checking for the presence of all json components is underway

--- exampreparation/preparation/views.py
+++ exampreparation/preparation/views.py
@@ -1,7 +1,12 @@
+from django.http import HttpResponseBadRequest
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class MobileApiView(APIView):
     def post(self, request):
-        return Response(request.data)  # Возвращаем json, который приходит с мобильного приложения для проверки
+        json = request.data
+        # Проверка на наличие всех компонентов
+        if all(json.get(key) for key in ['user_id', 'subject_name', 'file', 'time']):
+            return Response(json)  # Возвращаем json, который приходит с мобильного приложения для проверки
+        return HttpResponseBadRequest("Bad Request")
