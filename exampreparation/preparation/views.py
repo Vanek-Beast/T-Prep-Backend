31ec2b42b5dd30fd0fcd11240723ccc0eb95b325
ied9 <ied9@tpu.ru>
2024-11-01 22:47:46 +0700

    Add generate function

--- exampreparation/preparation/views.py
+++ exampreparation/preparation/views.py
@@ -1,12 +1,31 @@
 from django.http import HttpResponseBadRequest
 from rest_framework.response import Response
 from rest_framework.views import APIView
+import re
+
+
+def generate(content):
+    # Словарь для хранения вопросов и ответов
+    questions_dict = {}
+
+    for line in content.split("\n"):
+        # Пропускаем пустые строки
+        if not line.strip():
+            continue
+
+        # Убираем нумерацию
+        question = re.sub(r'^\d+[.)]\s*', '', line).strip()
+
+        # Добавляем вопрос и перевёрнутый текст как "ответ"
+        questions_dict[question] = question[::-1]
+
+    return questions_dict
 
 
 class MobileApiView(APIView):
     def post(self, request):
         json = request.data
         # Проверка на наличие всех компонентов
-        if all(json.get(key) for key in ['user_id', 'subject_name', 'file', 'time']):
-            return Response(json)  # Возвращаем json, который приходит с мобильного приложения для проверки
+        if all(json.get(key) for key in ['user_id', 'subject_name', 'file_content', 'time']):
+            return Response(generate(json.get('file_content')))
         return HttpResponseBadRequest("Bad Request")
