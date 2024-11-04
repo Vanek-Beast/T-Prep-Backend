4ce0751f7980f4c8311b0a36d3ab0e6bb3d35983
Dmitry <d.polonnikoff@yandex.ru>
2024-11-04 15:35:26 +0300

    Changed type of file field

--- exampreparation/exampreparation/settings.py
+++ exampreparation/exampreparation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-10l!_a&_82hm1g2dtrms%di*vc*=3tbbrb&8&hy0q&kahjk#1a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'exampreparation.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': 5432,
         'USER': 'admin',
         'PASSWORD': 'admin',
@@ -120,10 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
