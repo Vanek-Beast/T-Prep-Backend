35c40bd0eabefa10c8f006fd620ce4dbf9288287
ied9 <ied9@tpu.ru>
2024-11-07 16:52:16 +0700

    Fixed problems with API

--- exampreparation/preparation/views.py
+++ exampreparation/preparation/views.py
@@ -1,11 +1,11 @@
 import json
 
 from django.shortcuts import get_object_or_404
-from rest_framework import status, generics
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import re
-from .serializers import SubjectSerializer
+from .serializers import SubjectCreateSerializer, SubjectListSerializer
 from .models import Subject
 
 
@@ -36,17 +36,17 @@ class SubjectCreateView(APIView):
         if not uploaded_file:
             return Response({"error": "No file uploaded."}, status=status.HTTP_400_BAD_REQUEST)
         data['questions'] = generate(uploaded_file)
-        serializer = SubjectSerializer(data=data)
+        serializer = SubjectCreateSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response({"post": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"post": serializer.data}, status=status.HTTP_201_CREATED)
 
 
 class SubjectListView(APIView):
     def get(self, request, user_id):
         # Получаем список предметов для конкретного пользователя
         subjects = Subject.objects.filter(user_id=user_id)
-        serializer = SubjectSerializer(subjects, many=True)
+        serializer = SubjectListSerializer(subjects, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
