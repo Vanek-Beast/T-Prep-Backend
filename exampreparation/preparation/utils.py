43ceb823a1dbb33e7af24e4bb109477f900bb19f
ied9 <ied9@tpu.ru>
2024-11-16 01:45:38 +0700

    Improve prompt to get a better response, but it's still but the transmission is still in one stream

--- exampreparation/preparation/utils.py
+++ exampreparation/preparation/utils.py
@@ -1,5 +1,8 @@
-from langchain_gigachat.chat_models import GigaChat
+from langchain.chat_models import GigaChat
+from langchain.prompts import ChatPromptTemplate
 from config import api_key
+from langchain_core.output_parsers import StrOutputParser
+import yaml
 
 
 # Функция для создания экземпляра GigaChat
@@ -16,24 +19,25 @@ def create_llm(api_key):
 # Основная функция для общения с моделью
 def generate_answers(llm, questions):
     answers = {}
-    # Добавляем системное сообщение с начальной настройкой, если оно задано
-    prompt = "Ты - эксперт по подготовке к экзаменам. Тебе будет дан вопрос, " \
-             "на который необходимо дать краткий и точный ответ, учитывая каждый пункт, " \
-             "указанный в вопросе.В ответе необходимо отдать перевод в формате, приведенном ниже." \
-             "Ты ДОЛЖЕН дать четкий ответ на вопрос."
-    llm.invoke(prompt)
+    prompt = ChatPromptTemplate.from_template("Ты - эксперт по подготовке к экзаменам. "
+                                              "Тебе будет дан вопрос, на который необходимо дать краткий и точный "
+                                              "ответ, учитывая каждый пункт, указанный в вопросе."
+                                              "Ты ДОЛЖЕН дать четкий ответ на вопрос."
+                                              "Вопрос: {question}"
+                                              "Ответ:")
+    parser = StrOutputParser()
+    llm_chain = prompt | llm | parser
     for question in questions:
-        answer = llm.invoke(question)
-        answers[question] = answer.content
+        answer = llm_chain.invoke({"question": question})
+        answers[question] = answer
     return answers
 
 
 if __name__ == "__main__":
     llm = create_llm(api_key)
     questions = [
-        "Электрическое поле в вакууме. Напряженность электростатического поля. Принцип суперпозиции.",
-        "Электрический диполь. Напряженность поля на оси диполя.",
-        "Электрический диполь. Напряженность поля диполя на оси, лежащей на перпендикуляре, восстановленном к его середине.",
-        "Поток вектора напряженности. Теорема Остроградского-Гаусса."
+        "Основные цели и методы государственного регулирования.",
+        "Теория общественного выбора. Парадокс Кондорсе и теорема невозможности К.Эрроу.",
+        "Макроэкономическое равновесие в модели «AD-AS»."
     ]
-    print(generate_answers(llm, questions))
+    print(generate_answers(llm, questions).values())
