f460b34d114dca65358af5052d4d2248408e08f5
ied9 <ied9@tpu.ru>
2024-11-16 01:49:28 +0700

    Changed requirments.txt

--- exampreparation/preparation/utils.py
+++ exampreparation/preparation/utils.py
@@ -2,23 +2,18 @@ from langchain.chat_models import GigaChat
 from langchain.prompts import ChatPromptTemplate
 from config import api_key
 from langchain_core.output_parsers import StrOutputParser
-import yaml
 
 
-# Функция для создания экземпляра GigaChat
-def create_llm(api_key):
-    return GigaChat(
+# Основная функция для общения с моделью
+def generate_answers(questions):
+    answers = {}
+    llm = GigaChat(
         credentials=api_key,
         scope="GIGACHAT_API_PERS",
         model="GigaChat",
         verify_ssl_certs=False,
         streaming=False,
     )
-
-
-# Основная функция для общения с моделью
-def generate_answers(llm, questions):
-    answers = {}
     prompt = ChatPromptTemplate.from_template("Ты - эксперт по подготовке к экзаменам. "
                                               "Тебе будет дан вопрос, на который необходимо дать краткий и точный "
                                               "ответ, учитывая каждый пункт, указанный в вопросе."
@@ -34,10 +29,9 @@ def generate_answers(llm, questions):
 
 
 if __name__ == "__main__":
-    llm = create_llm(api_key)
     questions = [
         "Основные цели и методы государственного регулирования.",
         "Теория общественного выбора. Парадокс Кондорсе и теорема невозможности К.Эрроу.",
         "Макроэкономическое равновесие в модели «AD-AS»."
     ]
-    print(generate_answers(llm, questions).values())
+    print(generate_answers(questions).values())
